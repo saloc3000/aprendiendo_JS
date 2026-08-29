fa39eb285138ceee7677ee1ed1f2ded59c718aa1
saloc3000 <60454041+saloc3000@users.noreply.github.com>
2026-08-29 18:10:53 -0500

    añadir comentario de explicacion de los tres casos

--- js-for-react_pt4_funciones_anonimas/index.js
+++ js-for-react_pt4_funciones_anonimas/index.js
@@ -12,3 +12,23 @@ function start () {
 
 //  hacemos print en consola del resultado de la funcion
 console.log(start()) 
+
+// Una manera directa de hacerlo es pasarselo directamente el print
+
+console.log(
+    function start () {
+    return 'Iniciando  ...'
+} () 
+
+)
+// No es necesario darle el nombre a la funcion (podemos quitar el start) y de ahí viene lo de funcion anonima
+
+console.log(
+    function  () {
+    return 'Iniciando  ...'
+} () 
+
+)
+// Los tres ejemplos hacen lo mismo, sin embargo solo en el caso 1 la función queda declarada
+// (en los casos 2 y 3 no queda ninguna referencia a la función, así que no se puede volver
+// a llamar sin reescribir el código))
