1f003562f2bc927dcea9361fc36fef7c74322d89
saloc3000 <60454041+saloc3000@users.noreply.github.com>
2026-08-29 17:46:54 -0500

    explicacion destructuring ejemplo llaves

--- js-for-react_pt3/index.js
+++ js-for-react_pt3/index.js
@@ -28,9 +28,13 @@ function printInfo_v2(user){
 }
 document.body.innerHTML = printInfo_v2(user)
 
+// Bloque Destructuring Objects:
+// la idea central de Destructurar un objeto es poder usar solo las partes de objeto que nos interesan y usarlas como variables locales
 
-function printInfo_v3({name, age}){// Otra manera es usar los corchetes  para declarar que el objeto que le voy a pasar tiene esas propiedad si o si{propiedad 1, proiedad 2, ... propiedad_n} 
-    return '<h1> hola ' + name + '</h1> <h3> Tu edad es de: ' + age + '</h3>'  //lo que buscamos aquí es acceder al  atributo del objeto por medio de corcehetes 
+
+// Una manera es usar llaves  (keys) para acceder a los atributos del objeto. En este caso le decimos a la función que le vamos a pasar un objeto y que use las propiedades del objeto para asignar las variables locales con el mismo nombre de las keys.
+function printInfo_v3({name, age}){// Otra manera es usar las llaves  para declarar que el objeto que le voy a pasar tiene esas propiedadi{propiedad 1, proiedad 2, ... propiedad_n} 
+    return '<h1> hola ' + name + '</h1> <h3> Tu edad es de: ' + age + '</h3>'  //lo que buscamos aquí es acceder al  atributo del objeto sin necesidad de usar el nombre de objeto todo el tiempo (object.propiedad 1, object.proeidad_2, etc)
 
 
 }
