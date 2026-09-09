40413e5cc55e370dd305c145358d861eea92adde
saloc3000 <60454041+saloc3000@users.noreply.github.com>
2026-09-08 21:02:16 -0500

    otros ejemplos basicos

--- js-for-react_pt6_inline_functions/index.js
+++ js-for-react_pt6_inline_functions/index.js
@@ -12,3 +12,18 @@ console.log(add_V_arrow(2,3))
 const add_in_arrow = (x, y) =>  x +y
 console.log(add_in_arrow(2,3))
 
+// aqui par de ejemplos
+
+
+const showText = () => "Que mas"
+const showNumber = () => 50;
+const showarray = () => [1,2,3]
+const showBoolen = () => false;
+const showObject = () =>({'name':'Molly'})  // Debo usar lso paratensesis para usar el objecto
+
+console.log(showText())
+console.log(showNumber())
+console.log(showBoolen())
+console.log(showarray())
+console.log(showObject())
+
