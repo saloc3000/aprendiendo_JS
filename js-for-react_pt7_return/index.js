595fe4d547d7ed0773b648305a0828d299244767
saloc3000 <60454041+saloc3000@users.noreply.github.com>
2026-09-19 20:31:54 -0500

    añadir ejemplo de return

--- js-for-react_pt7_return/index.js
+++ js-for-react_pt7_return/index.js
@@ -1,56 +1,71 @@
-//  Las inline functions sirven para crear funciones arrow sin tener que poner explicitamente un return
-// // por ejemplo: 
+// SECCION RETURN 
 
-const add_V_arrow = (x, y) => {
+// return 200 : esto hace que la ejecucion termine inmediatemente.
+const button = document.createElement ("button")
 
-return x +y
+button.innerText="Click"
+document.body.append(button)
+
+
+const isAutorized = false; // cambiar a true o false para ver el efecto
+
+
+
+button.addEventListener("click", () =>{
+    if (isAutorized ==true) { //tampoco es necesario poner ==true, solo con if isAutorized es suficiente
+
+    return alert("Esta autorizado");
+    }
+    // no es necesario poner el else explicitamente
+    // else {
+    // alert("No esta autorizado");
+    // }
+
+    alert("No esta autorizado") // no es necesario poner el else explicitamente
+
 
 }
 
-console.log(add_V_arrow(2,3))
-// aqui no uso llaves ni el return porque  ya exist el return explicito
-const add_in_arrow = (x, y) =>  x +y
-console.log(add_in_arrow(2,3))
 
-// aqui par de ejemplos
+)
 
-
-const showText = () => "Que mas"
-const showNumber = () => 50;
-const showarray = () => [1,2,3]
-const showBoolen = () => false;
-const showObject = () =>({'name':'Molly'})  // Debo usar loo pareteesis para usar el objecto
-
-console.log(showText())
-console.log(showNumber())
-console.log(showBoolen())
-console.log(showarray())
-console.log(showObject()) // esto me trae el objeto
-console.log(showObject().name) // esto me trae el atributo de objeto
-
-//TODO: hacer ejemplos más complejos
-h1= document.createElement("h1")
-h1.innerText="Este es un Titulo Inicial "
-document.body.append(h1)
+const input=document.createElement('input')
+document.body.append(input)
+function añadirbienvenida()  {
+    if(edad <18) {
 
 
-// Como ejemplo Creare una función que siento que no es la mejor forma de usar un inlinefunction. Para mi las inline function tienen el proposito de ser usadas para funciones simples y legibles en una sola linea
 
-// Esta funcion crea un div le inserta el parametro texto a ese div y porsteriormente inserta ese  div antes del elemento seleccionado
-// Nota: la funcion Object.assign  lo que hace es: recibe un objeto (en este caso el div que esttoy creando) y le inserta (o remplaza) propiedades de ese objeto y me devuelve ese mismo objeto.
-const append_div_con_texto= (text, element_before) => document.body.insertBefore(Object.assign(document.createElement("div"),{'innerText':text}), element_before)
-//append_div_con_texto("pancho")
+        
 
-//  divv= document.createElement("div")
-//  divv.innerText="cola"
-//  console.log(divv)
-append_div_con_texto("DIV INSERTADO :D",h1)
+        return 20
+    }
+
+    const div=document.createElement("div")
+    div.innerText= "Bienvenido usuario de edad:" +edad
+    console.log("Bienvenido usuario de edad:" +edad)
+    document.body.append(div)
 
 
-append_div_con_texto("DIV INSERTADO") // si se deja vacio toma el ultimo elemento y lo inserta antes
 
-// UN ejemplo más correcto es crear una alerta al h1
+}
+let edad= null;
+input.addEventListener("change", ()=>{
+    if (input.value <18){
+    return alert("No Se permiten menores");}
+    
+    const valor=input.value
+    edad=valor
+    
+    
+    
+    return añadirbienvenida(edad)   
 
-h1.addEventListener('click' ,() => alert("autodestrucion iniciada")) // recibe el click y luego lo que hace despues (generar alerta de autodestruccion)
+}
+
+)
+
+
+
 
 
