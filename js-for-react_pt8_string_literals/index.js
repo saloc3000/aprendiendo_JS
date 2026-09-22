29aae4873ca3128ae00971324e1c97f27fcf955d
saloc3000 <60454041+saloc3000@users.noreply.github.com>
2026-09-21 19:26:37 -0500

    agregar directorio seccion string_literals

---
+++ js-for-react_pt8_string_literals/index.js
@@ -0,0 +1,71 @@
+// SECCION RETURN 
+
+// return 200 : esto hace que la ejecucion termine inmediatemente.
+const button = document.createElement ("button")
+
+button.innerText="Click"
+document.body.append(button)
+
+
+const isAutorized = false; // cambiar a true o false para ver el efecto
+
+
+
+button.addEventListener("click", () =>{
+    if (isAutorized ==true) { //tampoco es necesario poner ==true, solo con if isAutorized es suficiente
+
+    return alert("Esta autorizado");
+    }
+    // no es necesario poner el else explicitamente
+    // else {
+    // alert("No esta autorizado");
+    // }
+
+    alert("No esta autorizado") // no es necesario poner el else explicitamente
+
+
+}
+
+
+)
+
+const input=document.createElement('input')
+document.body.append(input)
+function añadirbienvenida()  {
+    if(edad <18) {
+
+
+
+        
+
+        return 20
+    }
+
+    const div=document.createElement("div")
+    div.innerHTML= '<h1> hola usuario '  + '</h1> <h3> Tu edad es de: ' + edad + '</h3>'  
+    console.log("Bienvenido usuario de edad:" +edad)
+    document.body.append(div)
+
+
+
+}
+let edad= null;
+input.addEventListener("change", ()=>{
+    if (input.value <18){
+    return alert("No Se permiten menores");}
+    
+    const valor=input.value
+    edad=valor
+    
+    
+    
+    return añadirbienvenida(edad)   
+
+}
+
+)
+
+
+
+
+
